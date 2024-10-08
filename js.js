00187c2ff549a20b87704d538ea7fa5900a9f97a
Sebas 7uu7 <smahechab@udistrital.edu.co>
2024-10-08 11:14:43 -0500

    mi primer commit

---
+++ js.js
@@ -0,0 +1,3 @@
+document.getElementById('infoButton').addEventListener('click', function() {
+    alert('¡La programación es una herramienta poderosa que te permite crear aplicaciones y resolver problemas!');
+});
